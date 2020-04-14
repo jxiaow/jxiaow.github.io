130558b7257165c51bcb5ed5333db28bd25eb554
ixiaow <i.xiaowujiang@gmail.com>
2020-04-14 15:32:57 +0000

    Travis CI Auto Builder :2020-04-14 23:32:57

---
+++ libs/codeBlock/codeBlockFuction.js
@@ -0,0 +1,3 @@
+// build time:Tue Apr 14 2020 15:32:48 GMT+0000 (Coordinated Universal Time)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
---
+++ libs/codeBlock/codeLang.js
@@ -0,0 +1,3 @@
+// build time:Tue Apr 14 2020 15:32:48 GMT+0000 (Coordinated Universal Time)
+$(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
+//rebuild by neat 
